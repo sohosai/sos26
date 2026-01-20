e9b7c8f3f142269af8064c2ce77d01d7fd240d6f
pocopota <pocopota.program@gmail.com>
2026-01-20 21:24:59 +0900

    コンポーネントのエクスポート

--- apps/web/src/components/primitives/index.ts
+++ apps/web/src/components/primitives/index.ts
@@ -1,1 +1,6 @@
 export { Button } from "./Button";
+export { Checkbox } from "./Checkbox";
+export { Radio } from "./Radio";
+export { Switch } from "./Switch";
+export { TextArea } from "./TextArea";
+export { TextField } from "./TextField";
