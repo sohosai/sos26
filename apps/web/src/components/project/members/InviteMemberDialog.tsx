82aa3e7dedffe61e4415796bc6a6f0c4bb41d99a
nAgI314 <kurosawasyunnagi@gmail.com>
2026-02-17 14:18:23 +0900

    招待コードのデザイン直し

--- apps/web/src/components/project/members/InviteMemberDialog.tsx
+++ apps/web/src/components/project/members/InviteMemberDialog.tsx
@@ -1,6 +1,7 @@
-import { Dialog } from "@radix-ui/themes";
-import { useContext } from "react";
-import { Button, TextField } from "@/components/primitives";
+import { Dialog, Popover, Text } from "@radix-ui/themes";
+import { IconCopy, IconX } from "@tabler/icons-react";
+import { useContext, useRef, useState } from "react";
+import { IconButton } from "@/components/primitives";
 import { ProjectContext } from "@/lib/project/context";
 import styles from "./InviteMemberDialog.module.scss";
 
@@ -11,6 +12,8 @@ type Props = {
 
 export function InviteMemberDialog({ open, onOpenChange }: Props) {
 	const project = useContext(ProjectContext);
+	const [copied, setCopied] = useState(false);
+	const timerRef = useRef<number | null>(null);
 
 	if (!project) return null;
 
@@ -19,19 +22,42 @@ export function InviteMemberDialog({ open, onOpenChange }: Props) {
 			<Dialog.Content>
 				<Dialog.Title>メンバーを招待</Dialog.Title>
 				<Dialog.Description>
-					以下の招待コードを共有してください
+					このコードを共有して、メンバーを招待できます
 				</Dialog.Description>
+				<div className={styles.closeButton}>
+					<IconButton onClick={() => onOpenChange(false)}>
+						<IconX size={16} />
+					</IconButton>
+				</div>
 				<div className={styles.content}>
-					<TextField value={project.inviteCode} disabled label={"招待コード"} />
+					<Text weight="bold" size="9" className={styles.inviteCode}>
+						{project.inviteCode}
+					</Text>
+					<Popover.Root open={copied}>
+						<Popover.Trigger>
+							<IconButton
+								onClick={() => {
+									navigator.clipboard.writeText(project.inviteCode);
 
-					<Button
-						intent="secondary"
-						onClick={() => {
-							navigator.clipboard.writeText(project.inviteCode);
-						}}
-					>
-						コピー
-					</Button>
+									setCopied(true);
+
+									if (timerRef.current) {
+										clearTimeout(timerRef.current);
+									}
+
+									timerRef.current = window.setTimeout(() => {
+										setCopied(false);
+									}, 2000);
+								}}
+							>
+								<IconCopy size={24} />
+							</IconButton>
+						</Popover.Trigger>
+
+						<Popover.Content side="top" align="center">
+							<Text size="2">コピーしました</Text>
+						</Popover.Content>
+					</Popover.Root>
 				</div>
 			</Dialog.Content>
 		</Dialog.Root>
